96efc930878ad489dea90280dbe7240b9ab284d7
Lukas Jorgensen <Lukas.r.jorgensen@gmail.com>
2019-12-03 12:50:59 -0500

    first commit

---
+++ js-guessing-number-game.js
@@ -0,0 +1,30 @@
+/* Number Guessing Game */
+
+
+// Variables
+var correctGuess = false;
+var randomNumber = Math.floor(Math.random() * 6) + 1;
+var firstGuess = prompt("Guess a random number between 1 and 6!")
+
+// Conditionals
+if (parseInt(firstGuess) === randomNumber) {
+    correctGuess = true;
+} 
+
+else if (firstGuess > randomNumber) {
+    var secondGuess = prompt('The number I am thinking of is less than ' + firstGuess + '.');
+    if (parseInt(secondGuess) === randomNumber) {
+        correctGuess = true;
+    }
+} 
+
+else if (firstGuess > randomNumber) {
+    var secondGuess = prompt('The number I am thinking of is more than ' + firstGuess + '.');
+    if (parseInt(secondGuess) === randomNumber) {
+        correctGuess = true;
+    }
+}
+
+if (correctGuess) {
+    document.write('<p>Boom! You guessed the number!</p>')
+}
