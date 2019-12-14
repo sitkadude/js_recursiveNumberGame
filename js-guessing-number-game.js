0093363ba23c7f57c1a07225788656ebeba6145e
Lukas Jorgensen <lukas.r.jorgensen@gmail.com>
2019-12-13 20:06:13 -0500

    fixed infinite bug

--- js-guessing-number-game.js
+++ js-guessing-number-game.js
@@ -6,15 +6,7 @@
 function guessNumber(randomNumber, message, maxGuesses, totalGuesses = 1) {
 
     if (totalGuesses > maxGuesses) {
-        const response = prompt("Sorry. The correct answer was " + randomNumber + "! Would you like to play again?");   
-    }
-
-    if (typeof response !== "undefined" && response.toUpperCase() === "NO") {
-        return "Thank you for playing!";
-    } 
-
-    if (typeof response !== "undefined" && response.toUpperCase() === "YES") {
-        guessNumber(randomNumber, "Guess a random number between 1 and 6!", 2);
+        return "Sorry. The correct answer was " + randomNumber + "!";  
     }
 
     const guess = prompt(message);
